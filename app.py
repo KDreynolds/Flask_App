7773e0725138074616ebdc69abaced7fcaa3c5d3
Kyle Reynolds <kylereynoldsdev@gmail.com>
2022-12-02 05:27:10 -0700

    Add files via upload
    adding login logic for /login endpoint.

--- app.py
+++ app.py
@@ -1,55 +1,45 @@
-import os
-import psycopg2
-from dotenv import load_dotenv
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask.templating import render_template
+from dotenv import load_dotenv
+import psycopg2
+import os
 
-CREATE_USERS_TABLE = (
-    "CREATE TABLE IF NOT EXISTS users (user_id SERIAL PRIMARY KEY, user_name TEXT UNIQUE, user_pass TEXT);"
-)
 
-CREATE_TIMELINE_TABLE = (
-    "CREATE TABLE IF NOT EXISTS timeline (post_id SERIAL PRIMARY KEY, post TEXT, post_user TEXT, date TIMESTAMP, FOREIGN KEY(post_user) REFERENCES users(user_name);"
-)
-
-CREATE_PROFILE_TABLE = (
-    "CREATE TABLE IF NOT EXISTS profiles (profile_id SERIAL PRIMARY KEY, bio TEXT, user_profile_id INTEGER, trophy INTEGER, FOREIGN KEY(user_profile_id) REFERENCES users(id);"
-)
-
-INSERT_USER = (
-    "INSERT INTO users(user_name, user_pass) VALUES (%s, %s);"
-)
-
-load_dotenv()
 
 app = Flask(__name__)
-url = os.getenv("DATABASE_URL")
-connection = psycopg2.connect(url)
 
-def db_factory():
-    CREATE_USERS_TABLE()
-    CREATE_PROFILE_TABLE()
-    CREATE_TIMELINE_TABLE()
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:frey@localhost:3306/test.db'
+
+DATABASE_URL=postgres://mfyjgndu:kHgJJW9PWazecpguvDDnWazf1KfLrckS@heffalump.db.elephantsql.com/mfyjgndu
 
 
+db = SQLAlchemy(app)
 
-@app.get("/")
+
+
+
+# Routes
+@app.route("/")
 def index():
     return render_template('login.html')
 
-@app.post("/signup")
-def create_profile():
-    data = request.get_json()
-    user_name = data["user_name"]
-    user_pass = data["user_pass"]
 
-    with connection:
-        with connection.cursor() as cursor:
-            cursor.execute(CREATE_USERS_TABLE)
-            cursor.execute(INSERT_USER, (user_name, user_pass))
-    return {"user name": user_name,}, 201
-    
+@app.route("/login", methods=['GET'])
+def login():
+    return
+
+
+@app.route("/signup", methods=['POST'])
+def create_profile():
+    user_name = request.form.get("user_name")
+    user_pass = request.form.get("user_pass")
+
+    if user_name != '' and user_pass != '':
+        with engine.begin() as connection:
+            result = connection.execute(Users.select())
+            connection.execute(Users.insert(), {"email": "email", "user_name": user_name, "user_pass": user_pass})
+
 
 
 @app.route("/timeline", methods=['GET'])
@@ -75,3 +65,8 @@ def create_bio():
 @app.route("/bio", methods=['GET'])
 def view_bio():
     return
+
+
+
+
+
